59844006ef2c1efdc2413b213dc7c2a07a970d98
s2choco <kyj_0129@naver.com>
2020-04-23 20:58:25 +0900

    feat: create detail activity UI

---
+++ app/src/main/java/com/foreknowledge/photomemo2/ui/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.foreknowledge.photomemo2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foreknowledge.photomemo2.R
+
+class DetailActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_detail)
+	}
+}
