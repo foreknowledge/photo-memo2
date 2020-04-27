bf9497358570f96985b6b0864b878b0bf1a53ff0
s2choco <kyj_0129@naver.com>
2020-04-28 01:40:14 +0900

    feat: Splash 화면 추가 및 이미지 작업 위한 설정 + Constants -> strings.xml로 리팩토링

--- app/src/main/java/com/foreknowledge/photomemo2/Constants.kt
+++ app/src/main/java/com/foreknowledge/photomemo2/Constants.kt
@@ -9,9 +9,6 @@ const val EXTRA_MEMO_ID = "memo_id"
 const val MAX_IMAGE_COUNT = 10
 
 const val MSG_IMAGE_FULL = "이미지 첨부는 ${MAX_IMAGE_COUNT}개까지만 가능합니다."
-const val MSG_VACANT_URL = "Url 주소를 입력해 주세요."
-const val MSG_NETWORK_DISCONNECT = "네트워크 연결 상태를 확인 해 주세요."
-const val MSG_VACANT_CONTENT = "입력한 내용이 없어 메모를 저장하지 않았어요."
 
 object RequestCode {
 	const val PERMISSION_REQUEST_CODE = 100
