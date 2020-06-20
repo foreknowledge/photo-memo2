a6d3c31af50714f0b35fb8165e1c4873ff003099
s2choco <kyj_0129@naver.com>
2020-06-20 19:12:40 +0900

    refactor: PreviewRecyclerAdapter 개별 리스트로 분리

--- app/src/main/java/com/foreknowledge/photomemo2/adapter/PreviewRecyclerAdapter.kt
+++ app/src/main/java/com/foreknowledge/photomemo2/adapter/PreviewRecyclerAdapter.kt
@@ -1,9 +1,9 @@
 package com.foreknowledge.photomemo2.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.foreknowledge.photomemo2.MAX_IMAGE_COUNT
 import com.foreknowledge.photomemo2.R
-import com.foreknowledge.photomemo2.base.BaseRecyclerAdapter
 import com.foreknowledge.photomemo2.base.BaseViewHolder
 import com.foreknowledge.photomemo2.databinding.ItemPreviewBinding
 import com.foreknowledge.photomemo2.listener.OnItemClickListener
@@ -14,19 +14,13 @@ import com.foreknowledge.photomemo2.util.FileUtil
 /**
  * Create by Yeji on 27,April,2020.
  */
-class PreviewRecyclerAdapter
-	: BaseRecyclerAdapter<String>(R.layout.item_preview), OnItemMoveListener {
-	private val originalImgPaths: List<String> = items
+class PreviewRecyclerAdapter : RecyclerView.Adapter<BaseViewHolder<String>>(), OnItemMoveListener {
+	private val items = mutableListOf<String>()
 	private val actionHistory = mutableListOf<Action>()
 
 	private data class Action(val type: Int, val imagePath: String)
 
-	private var onItemDragListener: OnItemDragListener =
-		object : OnItemDragListener {
-			override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
-				// default drag listener: do nothing
-			}
-		}
+	private var onItemDragListener: OnItemDragListener? = null
 
 	fun setOnItemDragListener(listener: (viewHolder: RecyclerView.ViewHolder) -> Unit) {
 		this.onItemDragListener = object : OnItemDragListener {
@@ -38,7 +32,7 @@ class PreviewRecyclerAdapter
 
 	fun getImages(): List<String> {
 		for (action in actionHistory)
-			if (action.type == DELETE_IMAGE)
+			if (action.type == FLAG_DELETE_IMAGE)
 				FileUtil.deleteFile(action.imagePath)
 
 		return items
@@ -46,21 +40,21 @@ class PreviewRecyclerAdapter
 
 	fun restoreImages(): List<String> {
 		for (action in actionHistory)
-			if (action.type == ADD_IMAGE)
+			if (action.type == FLAG_ADD_IMAGE)
 				FileUtil.deleteFile(action.imagePath)
 
-		return originalImgPaths
+		return items
 	}
 
 	fun addPath(path: String) {
-		actionHistory.add(Action(ADD_IMAGE, path))
+		actionHistory.add(Action(FLAG_ADD_IMAGE, path))
 		items.add(path)
 		notifyDataSetChanged()
 	}
 
 	fun addPaths(paths: List<String>) {
 		paths.forEach {
-			actionHistory.add(Action(ADD_IMAGE, it))
+			actionHistory.add(Action(FLAG_ADD_IMAGE, it))
 			items.add(it)
 		}
 		notifyDataSetChanged()
@@ -68,17 +62,22 @@ class PreviewRecyclerAdapter
 
 	fun isFull() = itemCount == MAX_IMAGE_COUNT
 
+	override fun getItemCount() = items.size
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+		object : BaseViewHolder<String>(R.layout.item_preview, parent) {}
+
 	override fun onBindViewHolder(holder: BaseViewHolder<String>, position: Int) {
 		holder.bind(items[position], object : OnItemClickListener<String>{
 			override fun onClick(item: String) {
-				actionHistory.add(Action(DELETE_IMAGE, item))
+				actionHistory.add(Action(FLAG_DELETE_IMAGE, item))
 				items.remove(item)
 				notifyDataSetChanged()
 			}
 		})
 
 		(holder.binding as ItemPreviewBinding).imagePreview.setOnLongClickListener {
-			onItemDragListener.onStartDrag(holder)
+			onItemDragListener?.onStartDrag(holder)
 			true
 		}
 	}
@@ -91,8 +90,15 @@ class PreviewRecyclerAdapter
 		notifyItemMoved(fromPosition, toPosition)
 	}
 
+	fun replaceItems(newItems: List<String>) {
+		items.clear()
+		items.addAll(newItems)
+		notifyDataSetChanged()
+	}
+
 	companion object {
-		const val ADD_IMAGE = 1
-		const val DELETE_IMAGE = -1
+		const val FLAG_ADD_IMAGE = 1
+		const val FLAG_DELETE_IMAGE = -1
 	}
 }
+
